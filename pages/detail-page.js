1c258d54cbd4c82b6e664e465b5da1327d972844
thinkbeforeyoucode <vanvelthovensebastiaan@gmail.com>
2020-01-10 14:33:21 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,4 +1,16 @@
-function sumbitComment() {
-    console.log("Hello")
-}
-console.log(sumbitComment())
+// add event handler
+function submitComment() {
+    // ...
+    
+    // create the elements you need
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    // adjust the elements we created
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    console.log(comment)
+  }
